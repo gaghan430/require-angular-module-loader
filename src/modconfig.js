f3cfae394e80a9e0c5d0e7cd25e5a6c1c3d96d08
Dodik Gaghan <gaghan430@gmail.com>
2016-08-08 21:26:29 +0700

    Check Asem

--- src/modconfig.js
+++ src/modconfig.js
@@ -2,7 +2,7 @@
  * @Author: gaghan
  * @Date:   2016-02-29 12:20:01
  * @Last Modified by:   Dodik Gaghan
- * @Last Modified time: 2016-08-08 20:53:14
+ * @Last Modified time: 2016-08-08 21:24:10
  */
 'use strict';
 define(['base'], function(base) {
@@ -13,7 +13,6 @@ define(['base'], function(base) {
             }
             return base.normalize(name, normalize);
         },
-
         load: function(name, req, onload, config) {
             base.validate(config, 'modconfig');
             var structure = config.structure;
