bf610765221a7295baaea1630d80932b6ec974eb
Dodik Gaghan <gaghan430@gmail.com>
2016-08-08 20:53:40 +0700

    Change Master 1

--- src/modconfig.js
+++ src/modconfig.js
@@ -2,11 +2,7 @@
  * @Author: gaghan
  * @Date:   2016-02-29 12:20:01
  * @Last Modified by:   Dodik Gaghan
-<<<<<<< 8f2cdd5960c36adf1789a7bd12beeff2d25b0b2c
- * @Last Modified time: 2016-08-08 20:52:13
-=======
- * @Last Modified time: 2016-08-08 19:51:11
->>>>>>> Change 1
+ * @Last Modified time: 2016-08-08 20:53:14
  */
 'use strict';
 define(['base'], function(base) {
@@ -17,7 +13,7 @@ define(['base'], function(base) {
             }
             return base.normalize(name, normalize);
         },
-  
+
         load: function(name, req, onload, config) {
             base.validate(config, 'modconfig');
             var structure = config.structure;
@@ -26,11 +22,7 @@ define(['base'], function(base) {
             if (structure.modconfig.hasOwnProperty('extension')) {
                 extension = structure.modconfig.extension;
             }
-<<<<<<< 8f2cdd5960c36adf1789a7bd12beeff2d25b0b2c
 
-=======
-            
->>>>>>> Change 1
             var path = structure.modconfig.path.replace(/{config}/g, modconfig).replace(/{extension}/g, extension);
             var module = base.module(name);
             var reqPath = base.path(path, config, base.getCurrentUrl(req), module);
