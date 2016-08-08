5d7716628abd01ac7d2181cc73bfa9d35e7c5992
Dodik Gaghan <gaghan430@gmail.com>
2016-08-08 19:51:23 +0700

    Change 1

--- src/modconfig.js
+++ src/modconfig.js
@@ -2,7 +2,11 @@
  * @Author: gaghan
  * @Date:   2016-02-29 12:20:01
  * @Last Modified by:   Dodik Gaghan
+<<<<<<< 8f2cdd5960c36adf1789a7bd12beeff2d25b0b2c
  * @Last Modified time: 2016-08-08 20:52:13
+=======
+ * @Last Modified time: 2016-08-08 19:51:11
+>>>>>>> Change 1
  */
 'use strict';
 define(['base'], function(base) {
@@ -22,7 +26,11 @@ define(['base'], function(base) {
             if (structure.modconfig.hasOwnProperty('extension')) {
                 extension = structure.modconfig.extension;
             }
+<<<<<<< 8f2cdd5960c36adf1789a7bd12beeff2d25b0b2c
 
+=======
+            
+>>>>>>> Change 1
             var path = structure.modconfig.path.replace(/{config}/g, modconfig).replace(/{extension}/g, extension);
             var module = base.module(name);
             var reqPath = base.path(path, config, base.getCurrentUrl(req), module);
