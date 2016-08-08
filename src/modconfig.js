244a8014667e9d22a210191ab2866d7459e88e63
Dodik Gaghan <gaghan430@gmail.com>
2016-08-08 20:53:40 +0700

    Change Master 1

--- src/modconfig.js
+++ src/modconfig.js
@@ -2,7 +2,7 @@
  * @Author: gaghan
  * @Date:   2016-02-29 12:20:01
  * @Last Modified by:   Dodik Gaghan
- * @Last Modified time: 2016-08-08 20:43:56
+ * @Last Modified time: 2016-08-08 20:53:14
  */
 'use strict';
 define(['base'], function(base) {
@@ -14,9 +14,6 @@ define(['base'], function(base) {
             return base.normalize(name, normalize);
         },
 
-
-
-        
         load: function(name, req, onload, config) {
             base.validate(config, 'modconfig');
             var structure = config.structure;
